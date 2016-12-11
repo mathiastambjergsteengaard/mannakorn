922aa68aed9475602798c52e552ad692757d2109
mathiastambjergsteengaard <mathiastambjergiversen@gmail.com>
2016-12-11 13:40:11 +0100

    fsds

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,8 +2,8 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += Product.all.map(&:image_ref).compact
-Rails.application.config.assets.precompile += ['footer.png']
+Rails.application.config.assets.precompile += ['footer.png', 'salme_37_5_road', 'salme_37_5_feet', 'esajas_53_4_6_thorn', 'esajas_53_4_6_cross', 'fadervor','salme_23_image', 'salme_23_white',
+'johannes_15_4']
 Rails.application.config.assets.precompile += ["application.scss", "order_show.scss"]
 
 # Add additional assets to the asset load path
