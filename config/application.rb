030a256376bec8ddc2403df722358ff773e19617
mathiastambjergsteengaard <mathiastambjergiversen@gmail.com>
2016-12-11 22:31:41 +0100

    fsd

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module JSLayout
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.middleware.use Mobvious::Manager
+    config.serve_static_assets = true
 
     config.active_record.raise_in_transactional_callbacks = true
   end
