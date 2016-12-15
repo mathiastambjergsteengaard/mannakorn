fcdb58f110a511f3dcb489c76d1f8822360f300e
mathiastambjergsteengaard <mathiastambjergiversen@gmail.com>
2016-12-15 09:51:45 +0100

    serve assets

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module JSLayout
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.middleware.use Mobvious::Manager
-    config.serve_static_files = true
 
     config.active_record.raise_in_transactional_callbacks = true
   end
